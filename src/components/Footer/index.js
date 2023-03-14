c1adf966359a5cf73a4c39d6f97eaa9e1c96a44d
Yure Rafael <112043267+yure07@users.noreply.github.com>
2023-03-14 18:21:24 -0300

    responive-design

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -19,6 +19,7 @@ export default function Footer () {
             <Link to='https://wa.link/eeecik' target='_blank'><BsWhatsapp/></Link>
     
          </div>
+         <p>Design By <a href='https://vstectity.com/' target='_blank'>Vstectity</a></p>
         </footer>
     )
 }
